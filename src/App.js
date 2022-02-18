af682f5ca048b7cc2e24820f27db54a66cc9aa95
Adolfo <scheidtadolfo@gmail.com>
2022-02-18 10:27:55 -0300

    implementando state var

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  // const message = "Hello world!";
+  const [message, setMessage] = useState("mensagem inicial");
 
-  const message = 'Hello world!';
   return (
-    <h1>{message}</h1>
+    <>
+      <div className="container">{message}</div>
+      <button onClick={() => setMessage("apertastes o botão, meus parabéns")}>
+        mudar mensagem
+      </button>
+    </>
   );
-
-}
+};
 
 export default App;
