44d7d2d7ff3719108a88c3524c18aef332b7da8e
Adolfo <scheidtadolfo@gmail.com>
2022-02-19 09:20:32 -0300

    adc botao de remover sem onclick

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,11 +3,20 @@ import "./Task.css";
 
 const Task = ({ task, handleTaskClick }) => {
   return (
-     <div className="task-container" style={task.completed ? { borderLeft: '6px solid rgb(238, 238, 13)'} : {}} onClick={() => handleTaskClick(task.id)}>
-          {task.title}
+    <div
+      className="task-container"
+      style={
+        task.completed ? { borderLeft: "6px solid rgb(238, 238, 13)" } : {}
+      }
+      onClick={() => handleTaskClick(task.id)}
+    >
+      {task.title}
+
+      <div className="buttons-container">
+        <button className="remove-task-button">X</button>
       </div>
-  )
-  // return <div className="task-container">{task.title}</div>;
+    </div>
+  );
 };
 
 export default Task;
